1d8dfd52e91705cd95a3fb955ab0b9f7f53b908c
ChocomintX <401162659@qq.com>
2021-04-02 21:54:06 +0800

    学习至4-4

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,126 @@
+package LinkedList;
+
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    int size;
+
+    public LinkedList() {
+        dummyHead = new Node(null, null);
+        size = 0;
+    }
+
+    //获取元素个数
+    public int getSize() {
+        return size;
+    }
+
+    //返回链表是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //在链表的index位置添加新的元素e
+    //不常用，仅练习
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++)
+            prev = prev.next;
+
+        prev.next = new Node(e, prev);
+        size++;
+    }
+
+    //在链表头部添加新的元素e
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    //在链表末尾添加新的元素e
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    //获得链表的第index个位置的元素
+    //不常用，仅练习
+    public E get(int index) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        return cur.e;
+    }
+
+    //获得链表的第一个元素
+    public E getFirst() {
+        return get(0);
+    }
+
+    //获得链表的最后一个一个元素
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    //修改链表的第index个位置的元素
+    //不常用，仅练习
+    public void set(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    //查找链表中是否存在元素e
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e))
+                return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+}
